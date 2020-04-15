f334bd712dd575d2c65b34561a3762025d830de4
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-15 20:54:06 -0300

    Entendendo variáveis no php

---
+++ variaveis.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome = "Rafael Morais";
+$idade = 28;
+
+echo $nome." tem a idade de: ".$idade;
+
+$nome = "Fulano da Silva";
+
+echo $nome." tem a idade de: ".$idade;
+
+$nome = 1000;
+var_dump($nome);
+
+?>
