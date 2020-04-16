6be547b5565f9ec55da4f85a915e6ab91aeab93b
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-16 15:29:27 -0300

    Praticando arrays associativos no php

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+
+$dados = ["nome" => "Rafael", "sobrenome" => "Morais", "idade" => 28, "maior" => true, "cores" => ["Preto", "Azul", "Vermelho"]];
+
+$dados['estiloMusical'] = ["MPB", "Rock", "Rap"];
+
+var_dump($dados);
+
+?>
