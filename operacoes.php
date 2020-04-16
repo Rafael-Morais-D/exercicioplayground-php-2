0de87210ac2753e57748aa0d55ff9ecff2c7deb5
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-16 15:45:21 -0300

    Praticando operadores no php

---
+++ operacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+$elemento1 = "Rafael";
+$elemento2 = "Morais";
+
+$resultado = 'Meu nome é '.$elemento1.' '.$elemento2;
+
+echo $resultado;
+
+// Adição +
+// Subtração -
+// Multiplicação *
+// Divisão /
+// Restante %
+// Potencia **
+
+?>
